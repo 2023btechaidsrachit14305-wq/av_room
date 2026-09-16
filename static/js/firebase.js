1e742702ef64e4c2947f63028cf2eee13d358a5a
Rachit Yogi <2023btechaidsrachit14305@poornima.edu.in>
2026-09-16 16:19:44 +0530

    Add Firebase web authentication client

---
+++ static/js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, getIdToken } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5--9ljxriqvIKASw_mvQP8UdU8q6B9ok",
+  authDomain: "avroom-7c3cb.firebaseapp.com",
+  projectId: "avroom-7c3cb",
+  storageBucket: "avroom-7c3cb.firebasestorage.app",
+  messagingSenderId: "963718871721",
+  appId: "1:963718871721:web:a42ff35586e8d4392807b8",
+  measurementId: "G-XQLJZV5NG"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+export { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, getIdToken };
